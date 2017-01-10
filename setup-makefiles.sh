e887164f371a03e372ea3ee32904a105c32d627e
Bruno Martins <bgcngm@gmail.com>
2017-01-10 22:17:02 +0000

    m7: Set device bring up year
     * To be inherited in m7-common
    
    Change-Id: I2aa73e6f91fbf08e20873605fcc6e1c3214c2204

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,5 +20,7 @@ set -e
 export DEVICE=m7
 export DEVICE_COMMON=m7-common
 export VENDOR=htc
+
+export DEVICE_BRINGUP_YEAR=2013
 
 ./../$DEVICE_COMMON/setup-makefiles.sh $@
