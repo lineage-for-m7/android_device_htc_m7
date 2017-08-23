1629c90fdd3a321f1de9af862dfbf419857f72a3
Bruno Martins <bgcngm@gmail.com>
2017-08-23 01:15:55 +0100

    m7: Inherit from Lineage vendor
    Change-Id: Ic43c494fa0d88503d42b6003b22cb8681ba7b7c8

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@
 $(call inherit-product, device/htc/m7/device.mk)
 
 # Inherit Lineage full phone configuration
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_m7
